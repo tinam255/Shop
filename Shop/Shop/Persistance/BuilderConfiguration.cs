1ccab7d4dec20c3d4f38413917cd54f652a6820b
Tina Mladenovic <tmladenovic99@gmail.com>
2024-06-01 11:02:59 +0200

    WIP

---
+++ Shop/Shop/Persistance/BuilderConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Persistance;
+
+public static class BuilderConfiguration
+{
+    public static void ApplyBuilderConfiguration(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Proizvod>(mb =>
+        {
+
+        });
+
+        modelBuilder.Entity<Dobavljac>(mb =>
+        {
+
+        });
+
+        modelBuilder.Entity<Proizvodjac>(mb =>
+        {
+
+        });
+    }
+}
---
+++ Shop/Shop/Persistance/ShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Persistance
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Proizvod> Proizvodi { get; set; }
+
+        public DbSet<Dobavljac> Dobavljaci { get; set; }
+
+        public DbSet<Proizvodjac> Proizvodjaci { get; set; }
+    }
+}
